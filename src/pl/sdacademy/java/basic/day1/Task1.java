828cec55a4edfa162eed098fcbd840c45f20558b
zygmunt7 <zygmunt.jasinski@pollub.edu.pl>
2022-04-02 11:26:07 +0100

    Solution Day1 exercise1

---
+++ src/pl/sdacademy/java/basic/day1/Task1.java
@@ -0,0 +1,36 @@
+package pl.sdacademy.java.basic.day1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        displayWordUsingDecimalFormat ();
+        System.out.println();
+        displayWordUsingBinaryFormat ();
+
+
+    }
+    private static void displayWordUsingDecimalFormat (){
+        char s = 83;
+        char d = 63;
+        char a = 65;
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
+    }
+    private static void displayWordUsingBinaryFormat (){
+        char s = 0b01010011;
+        char d = 0b01000100;
+        char a = 0b01000001;
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
+    }
+    private static void displayWordUsingBinaryFormat (){
+        char s = 0x53;
+        char d = 0x44;
+        char a = 0x41;
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
+    }
+
+}
