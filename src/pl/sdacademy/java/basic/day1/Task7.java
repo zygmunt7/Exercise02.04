c686fa71c64763ff0001d23fb7f2f11107d19160
zygmunt7 <zygmunt.jasinski@pollub.edu.pl>
2022-04-03 10:32:14 +0100

    Insert Task7

--- src/pl/sdacademy/java/basic/day1/Task7.java
+++ src/pl/sdacademy/java/basic/day1/Task7.java
@@ -1,2 +1,32 @@
-package pl.sdacademy.java.basic.day1;public class Task7 {
+package pl.sdacademy.java.basic.day1;
+
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+       String decisuin;
+       do {
+
+           System.out.print("Please instert first digit: ");
+           float firstDigit = scanner.nextFloat();
+           scanner.nextLine();
+           System.out.print("Please insert operator: ");
+           String operator = scanner.nextLine();
+           System.out.print("Please instert second digit: ");
+           float secondDigit = scanner.nextFloat();
+           scanner.nextLine();
+
+           float result = calculate(firstDigit, secondDigit, operator);
+           System.out.println("Result: " +result);
+
+           System.out.println("New calculation os Stop? ");
+           decisuin=scanner.nextLine();
+       }
+       while (!"Stop".equals(decisuin));
+    }
+
+    private static float calculate(float firstDigit, float secondDigit, String operator) {
+        return 0;
+    }
 }
