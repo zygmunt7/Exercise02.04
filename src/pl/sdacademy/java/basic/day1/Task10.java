21524a806db3d8670f9f82c662ed0bf971c11ff3
zygmunt7 <zygmunt.jasinski@pollub.edu.pl>
2022-04-06 22:04:42 +0100

    day1 Task10

--- src/pl/sdacademy/java/basic/day1/Task10.java
+++ src/pl/sdacademy/java/basic/day1/Task10.java
@@ -1,4 +1,21 @@
 package pl.sdacademy.java.basic.day1;
 
+import java.util.Scanner;
+
 public class Task10 {
+    public static void main(String[] args) {
+        int digit = getDigit();
+        int sum =0;
+        while (digit!=0){
+            sum += digit%10;
+            digit/=10;
+        }
+        System.out.print("Sum of digit: "+sum);
+
+    }
+    private static int getDigit (){
+        Scanner scanner =new Scanner(System.in);
+        System.out.print("Please insert digit: ");
+        return scanner.nextInt();
+    }
 }
